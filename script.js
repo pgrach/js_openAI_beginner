2576d33c2870f7a7aa88b71e220758df41d0e99b
Paul <gpu2003@gmail.com>
2023-07-16 23:04:32 +0100

    Add api foulder with server-side script, corrected client-side script

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 async function getResponse() {
-    const input = document.getElementById("userInput").value
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
-        method: "POST",
+    const userInput = document.getElementById("userInput").value
+    const response = await fetch('./api/getOpenAIResponse', {
+        method: 'POST',
         headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${process.env.OPENAI_KEY}`
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-            "model": "gpt-3.5-turbo",
-            "messages": [
-                { "role": "system", "content": "You are a helpful assistant." },
-                { "role": "user", "content": input }
-            ]
-        })
-    })
+        body: JSON.stringify({userInput}),
+    });
 
     const data = await response.json()
 
-
     // Display the assistant's response
-    document.getElementById("chatOutput").innerHTML += `<p>${data.choices[0].message.content}</p>`
+    document.getElementById("chatOutput").innerHTML += `<p>${data.response}</p>`
 }
