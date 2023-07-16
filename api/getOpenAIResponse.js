3db71537325ca7a1a1de66484e8c4293b338dd8c
Paul <gpu2003@gmail.com>
2023-07-16 23:17:13 +0100

    debuging servier-side script

---
+++ api/getOpenAIResponse.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch';
+
+export default async function handler(req, res) {
+    const { userInput } = req.body;
+
+    try {
+        const response = await fetch("https://api.openai.com/v1/chat/completions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${process.env.OPENAI_KEY}`
+            },
+            body: JSON.stringify({
+                "model": "gpt-3.5-turbo",
+                "messages": [
+                    { "role": "system", "content": "You are a helpful assistant." },
+                    { "role": "user", "content": userInput }
+                ]
+            })
+        });
+
+        if (!response.ok) {
+            // Log the response status and text for debugging
+            const text = await response.text();
+            console.error(`OpenAI API responded with ${response.status}: ${text}`);
+            throw new Error(`OpenAI API responded with ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        // Send the assistant's response back to the client
+        res.json({response: data.choices[0].message.content});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: error.message});
+    }
+};
